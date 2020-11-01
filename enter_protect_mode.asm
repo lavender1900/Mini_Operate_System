187c76635753a5c7a435ed21d2d6d655519ff3ef
gaojinrong <lavender_matrix@163.com>
2020-11-01 04:27:08 -0500

    Update include file, add Descriptor Attribute bit composition

--- enter_protect_mode.asm
+++ enter_protect_mode.asm
@@ -12,22 +12,49 @@ jmp	LABEL_BEGIN
 ; Global Descriptor Table
 			;    Base Address     Segment Limit      Attributes
 DUMMY:		Descriptor		0,		0,		0 	; First descriptor is not used
-DESC_CODE32:	Descriptor		0, 0ffffh,   		0; Code Segment, 32bit
-DESC_VIDEO:	Descriptor	0B8000h,  	   0ffffh,             	0; Video Segment, display text
+DESC_CODE32:	Descriptor      SEG_CODE32,	 0ffffh,   		SA_32BIT_EXE_ONLY_CODE; Code Segment, 32bit
+DESC_VIDEO:	Descriptor	0B8000h,  	   0ffffh,      SA_READ_WRITE_DATA; Video Segment, display text
 
 ; GDT end
 
 GDTLen 		equ	$ - DUMMY
 GDTPtr		dw	GDTLen - 1
-		dd	0
+		dd	DUMMY	
 
+; Selectors
 SelectorCode32	equ	DESC_CODE32 - DUMMY
 SelectorVideo	equ	DESC_VIDEO - DUMMY
 
+[section s16]
+[bits 16]
 LABEL_BEGIN:
-mov	ax, DUMMY
-xor	ebx, ebx
-mov	bx, cs
-shl	ebx, 4
-add	ebx, DUMMY
+; init stack
+mov 	sp, 0100h
+
+; load GDTR
+lgdt	[GDTPtr]
+
+cli
+
+in	al, 92h
+or	al, 10b
+out	92h, al
+
+mov	eax, cr0
+or	eax, 1
+mov	cr0, eax
+
+jmp	dword SelectorCode32: 0 ; jump into protect mode
+
+[section s32]
+[bits 32]
+SEG_CODE32:
+mov	ax, SelectorVideo
+mov	gs, ax
+
+mov	edi, (80 * 11 + 79) * 2
+mov	ah, 0Ch
+mov	al, 'P'
+mov	[gs:edi], ax
+
 jmp	$
